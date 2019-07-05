edb26bf3ec1e027b043c856cff365bf0792bcb38
Cameron2920 <caramsay@uwaterloo.ca>
2019-07-04 21:59:17 -0400

    Updated coalesce node.

--- lib/arel_extensions/nodes/coalesce.rb
+++ lib/arel_extensions/nodes/coalesce.rb
@@ -1,31 +1,11 @@
 module ArelExtensions
   module Nodes
     class Coalesce < Function
-	  RETURN_TYPE = :string
-      
-      attr_accessor :left_node_type
+      RETURN_TYPE = :string
 
       def initialize expr
-        tab = expr.map { |arg|
-          convert_to_node(arg)
-        }
-        case expr.first
-        when String
-          @left_node_type = :string
-        when Integer, Float
-          @left_node_type = :number
-        when ArelExtensions::Nodes::Coalesce, ArelExtensions::Nodes::Function
-          @left_node_type = expr.first.respond_to?(:left_node_type) ? expr.first.left_node_type : nil
-        when Arel::Nodes::Node, Arel::Attributes::Attribute 
-          @left_node_type = type_of_attribute(expr.first)
-        when Date
-          @left_node_type = :ruby_date
-        when DateTime, Time
-          @left_node_type = :ruby_time
-        end
-        return super(tab)
+        super(expr)
       end
-
     end
   end
 end
